0bf5c47d9b4eb58821491af3846dee100fd6ec63
Eman Alaa Khodary <104126088+Emanakh@users.noreply.github.com>
2023-04-29 02:05:44 +0200

    Add files via upload

---
+++ 02-exercise 19/index.js
@@ -0,0 +1,7 @@
+// practice with conditional statements.
+
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if (num % 2 == 0){console.log('even'); }
+   else if(num % 2 == 1){console.log('odd'); }  
+}
